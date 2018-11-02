41adcc64ac23be863f853186f7d94073863bfe77
Harsh Thakor <e2765@pha.pacificsource.com>
2018-11-01 22:41:58 -0700

    Add project files.

---
+++ FinanceApplication/FinanceApplication/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FinanceApplication.Models;
+
+namespace FinanceApplication.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
